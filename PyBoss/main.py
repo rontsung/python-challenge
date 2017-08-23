6dc325fc7b7a4f2347bd29c2f5febe4273c1db55
Ronald Tsung <Me@Ronald-Tsungs-MacBook-Pro.local>
2017-08-23 18:46:15 -0400

    uploaded pyparagraph files

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,43 +1,53 @@
 import os
 import csv
-import us_state_abbrev
-
-data1 = os.path.join("..", "Pythonday1", "employee_data1.csv")
-data2 = os.path.join("..", "Pythonday1", "employee_data2.csv")
-counter = 0
-current = data1
+# import dictionary of state abbreviations
+import us_state_abbrev as abbr
+# select csv
+data1 = os.path.join("employee_data1.csv")
+# information from csv will be classified and placed in these lists
 emp_id = []
 first = []
 last = []
 dob = []
 ssn = []
 state = []
-while counter < 2:
-    with open(current,'r') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=",")
-        for row in csvreader:
-            if row[1] == "Name":
-                continue
-            emp_id.append(row[0])
-            space = row[1].find(" ")
-            f = row[1][:space] 
-            first.append(f)
-            l = row[1][space+1:]
-            last.append(l)
-            year = row[2][:4]
-            month = row[2][5:7]
-            day = row[2][8:]
-            dob.append(month+'/'+day+'/'+year)
-            new_ssn = "***-**-"+row[3][7:]
-            ssn.append(new_ssn)
-            abb = us_state_abbrev.us_state_abbrev[row[4]]
-            state.append(abb)
-    current = data2
-    counter = counter + 1
+with open(data1,'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    # run through all rows of the csv
+    for row in csvreader:
+        # don't include the header in the classification
+        if row[1] == "Name":
+            continue
+        # classify each ID
+        emp_id.append(row[0])
+        # break each name into first and last, then add each to appropriate list
+        space = row[1].find(" ")
+        f = row[1][:space] 
+        first.append(f)
+        l = row[1][space+1:]
+        last.append(l)
+        # break the date into year, month, and day, and re-sort into appropriate format
+        year = row[2][:4]
+        month = row[2][5:7]
+        day = row[2][8:]
+        dob.append(month+'/'+day+'/'+year)
+        # blot out first digits of SSN and add to list
+        new_ssn = "***-**-"+row[3][7:]
+        ssn.append(new_ssn)
+        # use state abbreviation
+        a = abbr.us_state_abbrev[row[4]]
+        state.append(a)
+# zip the lists to a tuple
 total = zip(emp_id, first, last, dob, ssn, state)
+# new header
 header = ['Emp ID','First Name','Last Name','DOB','SSN','State']
+# output file
 output = os.path.join('boss.csv')
+# write the header and then the tuple to the output file
 with open(output, "w") as datafile:
     writer = csv.writer(datafile)
     writer.writerow(header)
     writer.writerows(total)
+
+
+
--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,27 +1,36 @@
 import os
-#import csv
-new_text = os.path.join("..", "Pythonday1", "Hello2.txt")
-counter = 0
+# choose text file to analyze
+new_text = os.path.join("paragraph_1.txt")
+# variables for sentence count and character count sans certain punctuation
+counter, sent_count = 0, 0
 with open(new_text, "r") as full:
     text = full.read()
-#with open(new_text) as new_text:
+    # split string of text into list of words
     word_list = text.split()
+    # " - " counts as a word and this annoyed me
     if "-" in word_list:
         word_list.pop(word_list.index("-"))
+    # count elements in word list
     word_count = len(word_list)
-    sent_count = 0
+    # runs through all characters in string
     for char in text:
+        # count sentences; doesn't cover exceptions like typos or middle initials
+        # remove the character from the letter count
         if char == "." or char == "!" or char == "?":
             sent_count = sent_count + 1
-        if char == " " or char == "." or char == "," \
-        or char == "?" or char == "!" or char == "'" or char == "-":
             counter = counter - 1
+        # add to letter count
         counter = counter + 1
+# change counter from integer to float or calculations will round off as integers
 counter = counter * 1.
+# avg letters per word
 let_per = counter / word_count
+# avg words per sentence, convert to float first
 sent_len = word_count * 1. / sent_count
-print("words: " + str(word_count), "sentences: \
-" + str(sent_count), "letter count per word: " + str(let_per), "avg sent len: " + str(sent_len))
-
-        
-
+# print results!
+print("Paragraph Analysis")
+print("-----------------")
+print("Approximate Word Count:", word_count) 
+print("Approximate Sentence Count:", sent_count)
+print("Average Letter Count:", let_per)
+print("Average Sentence Length:", sent_len)
